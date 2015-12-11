c75bf3a50425982907f8270a33da93b255d418e5
William Yu <williamy@andrew.cmu.edu>
2015-12-10 20:08:03 -0500

    add another screen, but no additional functionality

---
+++ app/src/main/java/io/wyu/thelastbakeoff/AppActivity.java
@@ -0,0 +1,17 @@
+package io.wyu.thelastbakeoff;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wyu on 12/10/15.
+ */
+public class AppActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.app_main);
+  }
+
+}
