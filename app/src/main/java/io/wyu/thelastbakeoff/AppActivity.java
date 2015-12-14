5318f2ba25813ca3c741fb18496814c7988b21e4
William Yu <williamy@andrew.cmu.edu>
2015-12-14 02:22:34 -0500

    bad commit habits again, but finished everything

--- app/src/main/java/io/wyu/thelastbakeoff/AppActivity.java
+++ app/src/main/java/io/wyu/thelastbakeoff/AppActivity.java
@@ -14,16 +14,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class AppActivity extends AppCompatActivity implements SensorEventListener {
   // class constants
+  private static final float X_THRESHOLD = 0.02f;
+  private static final float Y_THRESHOLD = 0.02f;
+  private static final long HOLD_TIME = 400;
+  private static final long VIBRATE_TIME = 100;
   private static final int TOTAL_TRIALS = 10;
   private static final int TOTAL_BUTTONS = 4;
+  private static final int LIGHT_THRESHOLD = 80;
 
-  private float prevZ = 1.0f;
+  private float prevLight;
   private long start, trialStart;
+  private long lightPrevChange;
   private long[] trialTimes;
   private int currTrial, currAction;
   private int targetButton, targetAction;
@@ -80,6 +87,7 @@ public class AppActivity extends AppCompatActivity implements SensorEventListene
     sm.registerListener(this, sr, SensorManager.SENSOR_DELAY_NORMAL);
   }
 
+  @Override
   protected void onPause() {
     // Be sure to register the sensor when the activity pauses
     // Prevents the sensor from draining the battery when not in use
@@ -90,50 +98,76 @@ public class AppActivity extends AppCompatActivity implements SensorEventListene
   private void onLightChanged(SensorEvent event) {
     float light = event.values[0];
 
-    // unlock and go to next button
-    if (locked && light > 20) {
-      locked = false;
-      currButton = (currButton + 1) % TOTAL_BUTTONS;
-      if (currButton == targetButton) {
-        v.vibrate(200);
-      }
-    // lock the current button
-    } else if (!locked && light <= 20) {
+    TextView temp = (TextView) findViewById(R.id.light);
+    temp.setText(String.format("Light Value: %f", light));
+
+    if (light < LIGHT_THRESHOLD && !locked) {
       locked = true;
-      if (currButton == targetButton) {
-        v.vibrate(500);
+      lightPrevChange = System.currentTimeMillis();
+    } else if (light < LIGHT_THRESHOLD) {
+      if (light != prevLight) {
+        if (System.currentTimeMillis() - lightPrevChange >= HOLD_TIME) {
+          currAction = 2;
+        }
+      }
+    } else if (light >= LIGHT_THRESHOLD && locked) {
+      locked = false;
+      // if user held the light down
+      if (System.currentTimeMillis() - lightPrevChange >= HOLD_TIME) {
+        currAction = 2;
+      } else {
+        currAction = 1;
+      }
+      if (currAction == targetAction && currButton == targetButton) {
+        nextTrial();
       }
     }
 
+    prevLight = event.values[0];
     updateScreen();
   }
 
   private void onRotationChanged(SensorEvent event) {
-    // have to fix this initialization case somehow
-    if (prevZ == 1.0) {
-      prevZ = event.values[2];
+    if (locked) return;
+
+    float x = event.values[0];
+    float y = event.values[1];
+    // might want to remove this later
+    float z = event.values[2];
+
+    TextView temp = (TextView) findViewById(R.id.rotation);
+    temp.setText(String.format("Rotation Values - x : %f, y : %f, z : %f", x, y, z));
+
+    if (x < -X_THRESHOLD && (currButton  == 1 || currButton == 3)) {
+      currButton--;
+      if (currButton == targetButton) {
+        v.vibrate(VIBRATE_TIME);
+      }
+    } else if (x >= X_THRESHOLD && (currButton == 0 || currButton == 2)) {
+      currButton++;
+      if (currButton == targetButton) {
+        v.vibrate(VIBRATE_TIME);
+      }
     }
 
-    // trying this out: if the user flips the phone "upwards"
-    if (prevZ - event.values[2] < -.03) {
-      if (currButton == targetButton && locked) {
-        currAction = 2;
+    if (y < -Y_THRESHOLD && (currButton == 0 || currButton == 1)) {
+      currButton+=2;
+      if (currButton == targetButton) {
+        v.vibrate(VIBRATE_TIME);
       }
-    // and downwards
-    } else if (prevZ - event.values[2] > .025) {
-      if (currButton == targetButton && locked) {
-        currAction = 1;
+    } else if (y >= Y_THRESHOLD && (currButton == 2 || currButton == 3)) {
+      currButton-=2;
+      if (currButton == targetButton) {
+        v.vibrate(VIBRATE_TIME);
       }
     }
 
-    prevZ = event.values[2];
     updateScreen();
   }
 
   private void updateScreen() {
     if (currAction == targetAction && currButton == targetButton && locked) {
       trialTimes[currTrial] = System.currentTimeMillis() - trialStart;
-      currTrial++;
       if (currTrial >= TOTAL_TRIALS) {
         Intent intent = new Intent(AppActivity.this, ResultsActivity.class);
         intent.putExtra("time", System.currentTimeMillis() - start);
@@ -141,11 +175,7 @@ public class AppActivity extends AppCompatActivity implements SensorEventListene
         intent.putExtra("results", trialTimes);
         AppActivity.this.startActivity(intent);
       } else {
-        trialStart = System.currentTimeMillis();
-        targetButton = random.nextInt(TOTAL_BUTTONS);
-        targetAction = random.nextBoolean() ? 1 : 2;
-        currButton = currAction = 0;
-        locked = false;
+        nextTrial();
       }
     }
     updateButtons();
@@ -168,6 +198,24 @@ public class AppActivity extends AppCompatActivity implements SensorEventListene
         temp.setBackgroundColor(Color.parseColor("#666666"));
         temp.setText(String.valueOf(i + 1));
       }
+    }
+  }
+
+  private void nextTrial() {
+    trialTimes[currTrial] = System.currentTimeMillis() - trialStart;
+    v.vibrate(100);
+    trialStart = System.currentTimeMillis();
+    targetButton = random.nextInt(TOTAL_BUTTONS);
+    targetAction = random.nextBoolean() ? 1 : 2;
+    currButton = currAction = 0;
+    locked = false;
+    currTrial++;
+    if (currTrial >= TOTAL_TRIALS) {
+      Intent intent = new Intent(AppActivity.this, ResultsActivity.class);
+      intent.putExtra("time", System.currentTimeMillis() - start);
+      intent.putExtra("trials", TOTAL_TRIALS);
+      intent.putExtra("results", trialTimes);
+      AppActivity.this.startActivity(intent);
     }
   }
 
--- app/src/main/java/io/wyu/thelastbakeoff/ResultsActivity.java
+++ app/src/main/java/io/wyu/thelastbakeoff/ResultsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
